a5e7934b6dec9aefa3cacc671077111c1a4d4ac5
jurufola <moulaye.haidara@laposte.net>
2021-07-12 11:22:22 +0200

    Creation entités Catégorie et Recette

---
+++ src/main/java/home/jurufola/recettecuisine/entities/Categorie.java
@@ -0,0 +1,97 @@
+package home.jurufola.recettecuisine.entities;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Classe représentant la catégorie
+ * @author juruf_000
+ */
+@Entity
+@Table(name = "categorie")
+public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @OneToMany(mappedBy = "categorie")
+    private Set<Recette> recettes = new HashSet<>();
+
+    /**
+     * Constructeur
+     * @param nom Le nm de la catégorie
+     * @param recettes Les recettes de la catégorie
+     */
+    public Categorie(String nom, Set<Recette> recettes) {
+        this.nom = nom;
+        this.recettes = recettes;
+    }
+
+    /**
+     * Constructeur vide
+     */
+    public Categorie() {
+    }
+
+    /**
+     * Gette id
+     * @return L'id de la catégorie
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Setter id
+     * @param id L'id de la catégorie
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter nom
+     * @return LE nom de la catégorie
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Setter nom
+     * @param nom Le nom de la catégorie
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * Getter recettes
+     * @return Les recettes de la catégorie
+     */
+    public Set<Recette> getRecettes() {
+        return recettes;
+    }
+
+    /**
+     * Setter recettes
+     * @param recettes Les recettes de la catégorie
+     */
+    public void setRecettes(Set<Recette> recettes) {
+        this.recettes = recettes;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorie{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", recettes=" + recettes +
+                '}';
+    }
+}
--- src/main/java/home/jurufola/recettecuisine/entities/Recette.java
+++ src/main/java/home/jurufola/recettecuisine/entities/Recette.java
@@ -1,4 +1,186 @@
 package home.jurufola.recettecuisine.entities;
 
+import javax.persistence.*;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "recette")
 public class Recette {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "categorie_id")
+    private Categorie categorie;
+
+    @Column(name = "type_cuisson")
+    private String typeCuisson;
+
+    private LocalTime tempsCuisson;
+
+    @ManyToMany
+    @JoinTable(name = "recette_ingredient",
+            joinColumns = {@JoinColumn(name = "recette_id")},
+            inverseJoinColumns = {@JoinColumn(name = "ingredient_id")})
+    private Set<Ingredient> ingredients = new HashSet<>();
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "nb_likes", columnDefinition = "integer default 0")
+    private int nbLikes;
+
+    /**
+     * Constructeur
+     * @param categorie La catégorie à laquelle appartient la recette
+     * @param typeCuisson Le type de cuisson de la recette
+     * @param tempsCuisson Le temps de cuisson de la recette
+     * @param ingredients Les ingrédients composant la recette
+     * @param description La description de la recette avec les différentes étapes
+     * @param nbLikes Le nombre de like de la recette
+     */
+    public Recette(Categorie categorie, String typeCuisson, LocalTime tempsCuisson, Set<Ingredient> ingredients,
+                   String description, int nbLikes) {
+        this.categorie = categorie;
+        this.typeCuisson = typeCuisson;
+        this.tempsCuisson = tempsCuisson;
+        this.ingredients = ingredients;
+        this.description = description;
+        this.nbLikes = nbLikes;
+    }
+
+    /**
+     * Constructeur vide
+     */
+    public Recette() {
+    }
+
+    /**
+     * Getter id
+     * @return L'id de la recette
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Setter id
+     * @param id L'id de la recette
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter categorie
+     * @return La catégorie de la recette
+     */
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    /**
+     * Setter categorie
+     * @param categorie La catégorie de la recette
+     */
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    /**
+     * Getter typeCuisson
+     * @return Le type de la cuisson
+     */
+    public String getTypeCuisson() {
+        return typeCuisson;
+    }
+
+    /**
+     * Setter typeCuisson
+     * @param typeCuisson Le type de la cuisson
+     */
+    public void setTypeCuisson(String typeCuisson) {
+        this.typeCuisson = typeCuisson;
+    }
+
+    /**
+     * Getter tempsCuisson
+     * @return Le temps de la cuisson
+     */
+    public LocalTime getTempsCuisson() {
+        return tempsCuisson;
+    }
+
+    /**
+     * Setter tempsCuisson
+     * @param tempsCuisson Le temps de la cuisson
+     */
+    public void setTempsCuisson(LocalTime tempsCuisson) {
+        this.tempsCuisson = tempsCuisson;
+    }
+
+    /**
+     * Getter ingredients
+     * @return Les ingrédients de la recette
+     */
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    /**
+     * Setter ingredients
+     * @param ingredients Les ingrédients de la recette
+     */
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    /**
+     * Getter description
+     * @return La description de la recette
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Setter description
+     * @param description La description de la recette
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Getter nbLikes
+     * @return Le nombre de likes de la recette
+     */
+    public int getNbLikes() {
+        return nbLikes;
+    }
+
+    /**
+     * Setter nbLikes
+     * @param nbLikes Le nombre de likes de la recette
+     */
+    public void setNbLikes(int nbLikes) {
+        this.nbLikes = nbLikes;
+    }
+
+    @Override
+    public String toString() {
+        return "Recette{" +
+                "id=" + id +
+                ", categorie=" + categorie +
+                ", typeCuisson='" + typeCuisson + '\'' +
+                ", tempsCuisson=" + tempsCuisson +
+                ", ingredients=" + ingredients +
+                ", description='" + description + '\'' +
+                ", nbLikes=" + nbLikes +
+                '}';
+    }
 }
